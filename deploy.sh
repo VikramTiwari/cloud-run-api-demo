d859fdc2c4082327f1ee8118d794f88548305157
Vikram Tiwari <vikramtheone1@gmail.com>
2019-09-24 08:11:44 -0700

    fix: update service name

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,3 @@
-docker build -t gcr.io/ivikramtiwari/cloud-run-api-demo .
-docker push gcr.io/ivikramtiwari/cloud-run-api-demo
-gcloud beta run deploy cloud-run-api-demo --image gcr.io/ivikramtiwari/cloud-run-api-demo --region us-central1 --platform managed
+docker build -t gcr.io/ivikramtiwari/cloud-run-api-demo-geo-from-ip .
+docker push gcr.io/ivikramtiwari/cloud-run-api-demo-geo-from-ip
+gcloud beta run deploy cloud-run-api-demo-geo-from-ip --image gcr.io/ivikramtiwari/cloud-run-api-demo-geo-from-ip --region us-central1 --platform managed
